73864cb203fc1d778c8f0403c4fbc11665b22a16
Jean THOMAS <git0@pub.jeanthomas.me>
2020-06-09 16:51:30 +0200

    New wishbone frontend

--- gram/frontend/wishbone.py
+++ gram/frontend/wishbone.py
@@ -13,7 +13,7 @@ class gramWishbone(Peripheral, Elaboratable):
     def __init__(self, core):
         super().__init__()
 
-        self._port = core.crossbar.get_port()
+        self._port = core.crossbar.get_port(data_width=32)
 
         dram_size = core.size//4
         dram_addr_width = log2_int(dram_size)
@@ -22,28 +22,47 @@ class gramWishbone(Peripheral, Elaboratable):
         self.bus = wishbone.Interface(addr_width=dram_addr_width,
                                       data_width=32, granularity=granularity)
 
-        map = MemoryMap(addr_width=dram_addr_width, data_width=granularity)
-        map.add_resource(self, size=dram_size)
+        map = MemoryMap(addr_width=dram_addr_width+log2_int(granularity)-1, data_width=granularity)
         self.bus.memory_map = map
 
     def elaborate(self, platform):
         m = Module()
 
-        ratio = wishbone_data_width//port_data_width
-        count = Signal(max=max(ratio, 2))
+        # Write datapath
+        m.d.comb += [
+            self._port.wdata.valid.eq(wishbone.cyc & wishbone.stb & wishbone.we),
+            self._port.wdata.data.eq(wishbone.dat_w),
+            self._port.wdata.we.eq(wishbone.sel),
+        ]
+
+        # Read datapath
+        m.d.comb += [
+            self.bus.dat_r.eq(self._port.rdata.data),
+            self._port.rdata.data.ready.eq(1),
+        ]
+
         with m.FSM():
             with m.State("Send-Cmd"):
                 m.d.comb += [
                     port.cmd.valid.eq(self.bus.cyc & self.bus.stb),
                     port.cmd.we.eq(self.bus.we),
-                    port.cmd.addr.eq(self.bus.adr*ratio + count - adr_offset),
+                    port.cmd.addr.eq(self.bus.adr),
                 ]
                 with m.If(port.cmd.valid & port.cmd.ready):
+                    with m.If(self.bus.we):
+                        m.next = "Wait-Write"
+                    with m.Else():
+                        m.next = "Wait-Read"
 
-                    #   with m.State("Write"):
-                    #       ...
+            with m.State("Wait-Read"):
+                with m.If(self._port.rdata.valid):
+                    m.d.comb += self.bus.ack.eq(1)
+                    m.next = "Send-Cmd"
 
-                    #   with m.State("Read"):
-                    #       ...
+            with m.State("Wait-Write"):
+                with m.If(self._port.wdata.ready):
+                    m.d.comb += self.bus.ack.eq(1)
+                    m.next = "Send-Cmd"
+
 
         return m
