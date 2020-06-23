75d74445ad141e94c01c9505559443567db03a66
Jean THOMAS <git0@pub.jeanthomas.me>
2020-06-23 16:10:12 +0200

    Fix DFII bitfield constants

--- libgram/src/dfii.h
+++ libgram/src/dfii.h
@@ -3,16 +3,16 @@
 
 #include <stdbool.h>
 
-#define DFII_CONTROL_SEL (1 << 1)
-#define DFII_CONTROL_CKE (1 << 2)
-#define DFII_CONTROL_ODT (1 << 3)
-#define DFII_CONTROL_RESET_N (1 << 4)
+#define DFII_CONTROL_SEL (1 << 0)
+#define DFII_CONTROL_CKE (1 << 1)
+#define DFII_CONTROL_ODT (1 << 2)
+#define DFII_CONTROL_RESET_N (1 << 3)
 
-#define DFII_COMMAND_CS (1 << 1)
-#define DFII_COMMAND_WE (1 << 2)
-#define DFII_COMMAND_CAS (1 << 3)
-#define DFII_COMMAND_RAS (1 << 4)
-#define DFII_COMMAND_WRDATA (1 << 5)
+#define DFII_COMMAND_CS (1 << 0)
+#define DFII_COMMAND_WE (1 << 1)
+#define DFII_COMMAND_CAS (1 << 2)
+#define DFII_COMMAND_RAS (1 << 3)
+#define DFII_COMMAND_WRDATA (1 << 4)
 
 void dfii_setsw(struct gramCtx *ctx, bool software_control);
 void dfii_initseq(struct gramCtx *ctx);
