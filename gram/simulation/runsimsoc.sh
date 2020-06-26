f869eb97a9fdb7ef9d086d295782f3e4304e6a7f
Jean THOMAS <git0@pub.jeanthomas.me>
2020-06-26 16:41:31 +0200

    Use FST instead of VCD

--- gram/simulation/runsimsoc.sh
+++ gram/simulation/runsimsoc.sh
@@ -8,4 +8,4 @@ iverilog -g2012 -s simsoctb -o simsoc simsoctb.v build/top.debug.v dram_model/dd
 	${LIB_DIR}/FD1S3AX.v ${LIB_DIR}/SGSR.v ${LIB_DIR}/ODDRX2F.v ${LIB_DIR}/ODDRX2DQA.v ${LIB_DIR}/DELAYF.v ${LIB_DIR}/BB.v ${LIB_DIR}/OB.v ${LIB_DIR}/IB.v \
 	${LIB_DIR}/DQSBUFM.v ${LIB_DIR}/UDFDL5_UDP_X.v ${LIB_DIR}/TSHX2DQSA.v ${LIB_DIR}/TSHX2DQA.v ${LIB_DIR}/ODDRX2DQSB.v ${LIB_DIR}/IDDRX2DQA.v DDRDLLA.patched.v \
 	${LIB_DIR}/CLKDIVF.v
-vvp simsoc -vcd
+vvp simsoc -fst-speed
