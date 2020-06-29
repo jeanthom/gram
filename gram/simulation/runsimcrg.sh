7efe902676a354fab60727db558631913439d1fb
Jean THOMAS <git0@pub.jeanthomas.me>
2020-06-29 14:40:55 +0200

    Use -n option in vvp to enable CTRL+C

--- gram/simulation/runsimcrg.sh
+++ gram/simulation/runsimcrg.sh
@@ -5,4 +5,4 @@ LIB_DIR=/usr/local/diamond/3.11_x64/ispfpga/verilog/data/ecp5u
 
 python simcrg.py generate simcrg.v
 iverilog -Wall -o simcrg simcrgtb.v simcrg.v ${LIB_DIR}/ECLKSYNCB.v ${LIB_DIR}/CLKDIVF.v ${LIB_DIR}/EHXPLLL.v ${LIB_DIR}/PUR.v ${LIB_DIR}/GSR.v
-vvp simcrg -vcd
+vvp -n simcrg -vcd
