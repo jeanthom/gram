50451abccd641f520863944096dd1239519d5b1c
Jean THOMAS <git0@pub.jeanthomas.me>
2020-07-08 15:35:18 +0200

    Add test case for AntiStarvation

---
+++ gram/test/test_core_multiplexer.py
@@ -0,0 +1,15 @@
+from nmigen import *
+
+from gram.core.multiplexer import _AntiStarvation
+from utils import *
+
+class AntiStarvationTestCase(FHDLTestCase):
+    def test_formal(self):
+        def generic_test(timeout):
+            dut = _AntiStarvation(timeout)
+            self.assertFormal(dut, mode="bmc", depth=4)
+
+        generic_test(0)
+        generic_test(1)
+        generic_test(5)
+        generic_test(10)
