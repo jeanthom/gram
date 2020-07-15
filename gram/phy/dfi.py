f36314cc3e4393848b7aee78b052d1878a748ba2
Jean THOMAS <git0@pub.jeanthomas.me>
2020-07-15 12:02:27 +0200

    Fix code styling

--- gram/phy/dfi.py
+++ gram/phy/dfi.py
@@ -11,23 +11,23 @@ def phase_description(addressbits, bankbits, nranks, databits):
     return [
         # cmd description
         ("address", addressbits, DIR_FANOUT),
-        ("bank",       bankbits, DIR_FANOUT),
-        ("cas_n",             1, DIR_FANOUT),
-        ("cs_n",         nranks, DIR_FANOUT),
-        ("ras_n",             1, DIR_FANOUT),
-        ("we_n",              1, DIR_FANOUT),
-        ("clk_en",          nranks, DIR_FANOUT),
-        ("odt",          nranks, DIR_FANOUT),
-        ("reset_n",           1, DIR_FANOUT),
-        ("act_n",             1, DIR_FANOUT),
+        ("bank", bankbits, DIR_FANOUT),
+        ("cas_n", 1, DIR_FANOUT),
+        ("cs_n", nranks, DIR_FANOUT),
+        ("ras_n", 1, DIR_FANOUT),
+        ("we_n", 1, DIR_FANOUT),
+        ("clk_en", nranks, DIR_FANOUT),
+        ("odt", nranks, DIR_FANOUT),
+        ("reset_n", 1, DIR_FANOUT),
+        ("act_n", 1, DIR_FANOUT),
         # wrdata description
-        ("wrdata",         databits, DIR_FANOUT),
-        ("wrdata_en",             1, DIR_FANOUT),
+        ("wrdata", databits, DIR_FANOUT),
+        ("wrdata_en", 1, DIR_FANOUT),
         ("wrdata_mask", databits//8, DIR_FANOUT),
         # rddata description
-        ("rddata_en",           1, DIR_FANOUT),
-        ("rddata",       databits, DIR_FANIN),
-        ("rddata_valid",        1, DIR_FANIN),
+        ("rddata_en", 1, DIR_FANOUT),
+        ("rddata", databits, DIR_FANIN),
+        ("rddata_valid", 1, DIR_FANIN),
     ]
 
 
