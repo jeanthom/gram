1e4c06880e9076d400f5b56e833d5f0c824f42dd
Jean THOMAS <git0@pub.jeanthomas.me>
2020-07-17 17:22:27 +0200

    Add test for _AntiStarvation timer duration

--- gram/test/test_core_multiplexer.py
+++ gram/test/test_core_multiplexer.py
@@ -4,6 +4,25 @@ from gram.core.multiplexer import _AntiStarvation
 from utils import *
 
 class AntiStarvationTestCase(FHDLTestCase):
+    def test_duration(self):
+        def generic_test(timeout):
+            m = Module()
+            m.submodules = dut = _AntiStarvation(timeout)
+
+            def process():
+                yield dut.en.eq(1)
+                yield
+                yield dut.en.eq(0)
+                yield
+
+                for i in range(timeout):
+                    self.assertFalse((yield dut.max_time))
+                    yield
+
+                self.assertTrue((yield dut.max_time))
+
+            runSimulation(m, process, "test_core_multiplexer_antistarvation.vcd")
+
     def test_formal(self):
         def generic_test(timeout):
             dut = _AntiStarvation(timeout)
@@ -13,3 +32,4 @@ class AntiStarvationTestCase(FHDLTestCase):
         #generic_test(1)
         generic_test(5)
         generic_test(10)
+        generic_test(0x20)
