8a4080ee463735f59ae2891ab9b488ac86ec8cef
Jean THOMAS <git0@pub.jeanthomas.me>
2020-07-24 16:34:58 +0200

    Add test for tFAWController

--- gram/test/test_common.py
+++ gram/test/test_common.py
@@ -2,7 +2,7 @@
 from nmigen import *
 from nmigen.hdl.ast import Past
 
-from gram.common import tXXDController
+from gram.common import tXXDController, tFAWController
 from utils import *
 
 class tXXDControllerTestCase(FHDLTestCase):
@@ -16,3 +16,20 @@ class tXXDControllerTestCase(FHDLTestCase):
         generic_test(1)
         generic_test(5)
         generic_test(10)
+
+class tFAWControllerTestCase(FHDLTestCase):
+    def test_strobe_3(self):
+        dut = tFAWController(10)
+        def process():
+            yield dut.valid.eq(1)
+            
+            for i in range(3):
+                self.assertTrue((yield dut.ready))
+                yield
+
+            yield dut.valid.eq(0)
+            yield
+
+            self.assertFalse((yield dut.valid))
+
+        runSimulation(dut, process, "test_common.vcd")
