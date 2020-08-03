d92f520fc8eec53d01b37e481f0aa4db93195572
Jean THOMAS <git0@pub.jeanthomas.me>
2020-08-03 13:35:50 +0200

    Update setup.py (based on LUNA)

--- setup.py
+++ setup.py
@@ -5,15 +5,41 @@ from setuptools import find_packages
 
 
 setup(
+    # Vitals
     name="gram",
-    description="DRAM core for LambdaSoC",
-    author="LambdaConcept",
-    author_email="contact@lambdaconcept.com",
+    license="BSD",
     url="https://lambdaconcept.com",
     download_url="https://github.com/lambdaconcept/gram",
-    license="BSD",
-    python_requires="~=3.6",
-    install_requires=[],
+    author="LambdaConcept",
+    author_email="contact@lambdaconcept.com",
+    description="DRAM core for LambdaSoC",
+    use_scm_version= {
+        "root": '..',
+        "relative_to": __file__,
+        "version_scheme": "guess-next-dev",
+        "local_scheme": lambda version : version.format_choice("+{node}", "+{node}.dirty"),
+        "fallback_version": "r0.0"
+    },
+
+    # Imports / exports / requirements
+    platforms='any',
     packages=find_packages(exclude=("test*", "doc*", "examples*", "contrib*", "libgram*")),
     include_package_data=True,
+    python_requires="~=3.7",
+    install_requires=['nmigen', 'nmigen_boards', 'pyvcd'],
+    setup_requires=['setuptools', 'setuptools_scm'],
+    entry_points={},
+
+    # Metadata
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 1 - Planning',
+        'Natural Language :: English',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        ],
 )
